07c6e0af7c52519ed110f6e6d1508052b6235fbd
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-08-28 20:57:48 +0900

    2020 8 28

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import './App.css'
 import React from 'react'
+import { useAlert } from 'react'
 
 function App() {
   const handleClick = () => {
     console.log("Hello, world!!!")
-
+    const alert = useAlert
   }
+
   return (
     <div>
       <button onClick={handleClick}>
      牡丹
     </button>
+       <button onClick={() => {alert("Hello, world!!!") }}>
+      アラート
+    </button>
+    <button onClick={handleClick}>
+     いいね
+    </button>
+    <button onClick={handleClick}>
+        よくないね
+    </button>
+    <button onClick={handleClick}>
+        リセット
+    </button>
     </div>
   )
 }
-
 
 export default App;
