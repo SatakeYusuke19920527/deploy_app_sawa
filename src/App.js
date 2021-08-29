1a6a343ea0c98c78327bada6b730663baeb562a5
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-08-29 20:57:53 +0900

    2021 8 29

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import './App.css'
 import React from 'react'
-import { useAlert } from 'react'
+import { useAlert ,useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
   const handleClick = () => {
     console.log("Hello, world!!!")
     const alert = useAlert
-  }
+  };
+    
+     const increment = () => {
+       setCount(count + 1);
+    };
 
-  return (
+     const decrement = () => {
+      setCount(count - 1);
+    };
+
+     const reset = () => {
+      setCount(0);
+    };
+
+    return(
     <div>
-      <button onClick={handleClick}>
-     牡丹
-    </button>
-       <button onClick={() => {alert("Hello, world!!!") }}>
-      アラート
-    </button>
-    <button onClick={handleClick}>
-     いいね
-    </button>
-    <button onClick={handleClick}>
-        よくないね
-    </button>
-    <button onClick={handleClick}>
-        リセット
-    </button>
+    <button onClick={handleClick}>牡丹</button>
+    <button onClick={() => { alert("Hello, world!!!") }}>アラート</button>
+    <button onClick={increment}>いいね</button>
+    <button onClick={decrement}>よくないね</button>
+    <button onClick={reset}>リセット</button>
     </div>
   )
-}
+};
 
 export default App;
